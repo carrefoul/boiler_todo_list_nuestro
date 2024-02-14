624db98e7a053289879e26a48aecd6b5ab6981f1
carrefoul <mrotaru5206@esdmadrid.es>
2024-02-14 18:49:36 +0100

    First commit

--- app/main.js
+++ app/main.js
@@ -2,16 +2,58 @@ import { tareas } from '../assets/data/tareas.js';
 
 window.onload = () => {
     let lista = document.querySelector("#lista");
+    let idContador = 1;
 
-    tareas.forEach((tarea) => {
-        let elem = `
-            <li class="lista_elemento" id="${tarea.id}">
-                <p>${tarea.titulo}</p>
-                <button class="btn_cerrar">X</button>
-            </li>
+    function renderizarTareas() {
+        lista.innerHTML = '';
+        tareas.forEach((tarea) => {
+            let elem = `
+                <li class="lista_elemento" id="${tarea.id}">
+                    <p>${tarea.titulo}</p>
+                    <button class="btn_cerrar">X</button>
+                </li>
+            `;
+            lista.innerHTML += elem;
+        });
+    }
+
+    function crearModal() {
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <div class="modal-content">
+                <input type="text" id="nuevaTareaInput" placeholder="Nueva tarea">
+                <button id="agregarNuevaTareaBtn">Añadir</button>
+            </div>
         `;
-        console.log(elem)
-        lista.innerHTML+=elem;
-    })
-}
+        document.body.appendChild(modal);
 
+        let agregarNuevaTareaBtn = modal.querySelector('#agregarNuevaTareaBtn');
+        agregarNuevaTareaBtn.addEventListener('click', () => {
+            let nuevoTitulo = document.querySelector('#nuevaTareaInput').value.trim();
+            if (nuevoTitulo !== '') {
+                const nuevaTarea = {
+                    id: idContador++,
+                    titulo: nuevoTitulo
+                };
+                tareas.push(nuevaTarea);
+                renderizarTareas();
+                document.querySelector('#nuevaTareaInput').value = '';
+            }
+        });
+
+        return modal;
+    }
+
+    crearModal();
+
+    renderizarTareas();
+
+    lista.addEventListener('click', (event) => {
+        if (event.target.classList.contains('btn_cerrar')) {
+            let tareaId = event.target.parentElement.id;
+            tareas = tareas.filter(tarea => tarea.id != tareaId);
+            renderizarTareas();
+        }
+    });
+};
